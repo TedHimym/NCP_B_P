eead9d75d5fc28c23d880b346fb748538c923ea1
class_pc <mfpczs@outlook.com>
2020-12-16 12:51:02 +0800

    1216

--- inject_an.m
+++ inject_an.m
@@ -32,10 +32,11 @@ U = mU(:,end);
 for indexd = 1:length(t_c)
     time = t_c{indexd};
     eL = L(indexd, :);
+    plot(time/steady_time(indexd), eL, '-.')
     Q = (U(indexd).*delta_ios(indexd).*(R(indexd)+delta_ios(indexd).*0.15));
     uin_d_1 = (R(indexd)+eL).*(eL.^3)./(Q.*(g(indexd)*0.0016*7));
 %     plot(time, power(uin_d_1, 1/3), '.')
 %     uin_d_1 = (R(indexd)+eL).*(eL.^3)./(mU(indexd).*theta(indexd).*(g(indexd)*0.0016*7).*(R(indexd)+theta(indexd).*0.15));
-    plot(time./steady_time(indexd), power(uin_d_1, 1/3)./steady_time(indexd), '.')
+%     plot(time./steady_time(indexd), power(uin_d_1, 1/3)./steady_time(indexd), '.')
 %     pause
 end
